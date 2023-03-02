26024a86e09e238ef3efdd8873b502d000928fc9
Reggeditt <tkodjosarso@gmail.com>
2023-03-02 17:00:05 +0000

    implement completed status and edit task tests

--- src/__mock__/todoListData.js
+++ src/__mock__/todoListData.js
@@ -22,6 +22,56 @@ class TodoListData {
     return this.todoListTasks;
   };
 
+  editTaskDescription = (todoListWrapperElement) => {
+    const todoListWrapperChildren = Array.from(todoListWrapperElement.children);
+    const listDescriptionArr = document.getElementsByClassName('todo-list-item-description');
+    const inputFields = Array.from(listDescriptionArr);
+    inputFields.forEach((input, i) => {
+      input.addEventListener('click', () => {
+        if (document.activeElement === input) {
+          todoListWrapperElement.children[i].style.backgroundColor = 'yellow';
+          todoListWrapperElement.children[i].children[2].style.display = 'block';
+          input.style.backgroundColor = 'yellow';
+          todoListWrapperChildren[i].style.setProperty('--before', 'none');
+        } else {
+          todoListWrapperElement.children[i].style.backgroundColor = 'white';
+          todoListWrapperElement.children[i].children[2].style.display = 'none';
+          input.style.backgroundColor = 'white';
+          todoListWrapperChildren[i].style.setProperty('--before', 'block');
+        }
+      });
+
+      input.addEventListener('change', () => {
+        this.todoListTasks[i].description = input.value;
+        todoListWrapperElement.children[i].style.backgroundColor = 'white';
+        todoListWrapperElement.children[i].children[2].style.display = 'none';
+        input.style.backgroundColor = 'white';
+        todoListWrapperChildren[i].style.setProperty('--before', 'block');
+        document.getElementById('todo-input').focus();
+        this.updateStorage();
+      });
+    });
+    return this.todoListTasks;
+  };
+
+  setCompletedStatus = (todoListWrapperElement) => {
+    const todoListWrapperChildren = Array.from(todoListWrapperElement.children);
+    todoListWrapperChildren.forEach((child, index) => {
+      child.children[0].addEventListener('click', () => {
+        if (child.children[0].checked) {
+          child.children[1].style.textDecoration = 'line-through';
+          this.todoListTasks[index].isCompleted = true;
+          this.updateStorage();
+        } else {
+          child.children[1].style.textDecoration = 'none';
+          this.todoListTasks[index].isCompleted = false;
+          this.updateStorage();
+        }
+        return this.todoListTasks;
+      });
+    });
+  };
+
   clear = () => {
     this.todoListTasks = [];
   };
---
+++ src/statusCheck.test.js
@@ -0,0 +1,49 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const TodoListData = require('./__mock__/todoListData.js');
+
+describe('update completed status', () => {
+  const todoListDataTest = new TodoListData();
+  test('Check setCompletedStatus able to update completed status', () => {
+    document.body.innerHTML = `
+    <ul class="todo-list-wrap" id="todo-list-wrap">
+      <li class="todo-list-item" id="list-item1" draggable="true">
+        <input type="checkbox" id="checkbox1" data-id="1" class="task-checkbox">
+        <input type="text" class="todo-list-item-description" style="text-decoration: line-through;">
+        <button class="todo-list-item-remove" data-id="1"></button>
+      </li>
+      <li class="todo-list-item" id="list-item2" draggable="true">
+        <input type="checkbox" id="checkbox2" data-id="2" class="task-checkbox">
+        <input type="text" class="todo-list-item-description" style="text-decoration: none;">
+        <button class="todo-list-item-remove" data-id="2"></button>
+      </li>
+    </ul>`;
+
+    const taskListWrapperElement = document.getElementById('todo-list-wrap');
+    expect(todoListDataTest.setCompletedStatus(taskListWrapperElement)).toBe(this.todoListTasks);
+  });
+});
+
+describe('editTaskDescription', () => {
+  const todoListDataTest = new TodoListData();
+  test('check edit task description function', () => {
+    document.body.innerHTML = `
+    <ul class="todo-list-wrap" id="todo-list-wrap">
+      <li class="todo-list-item" id="list-item1" draggable="true">
+        <input type="checkbox" id="checkbox1" data-id="1" class="task-checkbox">
+        <input type="text" class="todo-list-item-description" style="text-decoration: line-through;">
+        <button class="todo-list-item-remove" data-id="1"></button>
+      </li>
+      <li class="todo-list-item" id="list-item2" draggable="true">
+        <input type="checkbox" id="checkbox2" data-id="2" class="task-checkbox">
+        <input type="text" class="todo-list-item-description" style="text-decoration: none;">
+        <button class="todo-list-item-remove" data-id="2"></button>
+      </li>
+    </ul>`;
+    const taskListWrapperElement = document.getElementById('todo-list-wrap');
+    const currentTaskArray = todoListDataTest.todoListTasks;
+    expect(todoListDataTest.editTaskDescription(taskListWrapperElement)).toBe(currentTaskArray);
+  });
+});
