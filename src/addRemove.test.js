6019a89f224c2c49227461e036687c29e30d9c6d
Reggeditt <tkodjosarso@gmail.com>
2023-03-02 11:42:54 +0000

    add html mock

--- src/addRemove.test.js
+++ src/addRemove.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const TodoListData = require('./__mock__/todoListData.js');
 
 describe('addTask', () => {
@@ -9,5 +13,32 @@ describe('addTask', () => {
       taskIndex: 1,
     };
     expect(todoListDataTest.addTask(newTask)).toEqual(todoListDataTest.todoListTasks);
+  });
+
+  test('Check addTodo able add todo as html to todoList display', () => {
+    document.body.innerHTML = `
+      <form id="form" class="form" action="">
+        <input type="text" id="todo-input" placeholder="Add to your list" autofocus/>
+        <button id="enter-input" title="add task"></button>
+      </form>
+      <ul class="todo-list-wrap" id="todo-list-wrap"></ul>
+    `;
+
+    const todoListWrapperElement = document.getElementById('todo-list-wrap');
+    const formElement = document.getElementById('form');
+    const inputElement = document.getElementById('todo-input');
+
+    inputElement.value = 'New todolist!';
+    formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const newTask = {
+        description: inputElement.value,
+        isCompleted: false,
+        taskIndex: null,
+      };
+      todoListDataTest.addTask(newTask);
+      todoListWrapperElement.innerHTML = `<li>${todoListDataTest.todoListTasks[0].description}</li>`;
+      expect(todoListWrapperElement.innerHTML).toBe('<li>New todolist!</li>');
+    });
   });
 });
